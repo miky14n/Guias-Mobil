a81aa91b110b73d3cad524cb270e412a433613c2
miky14n <miky14n@gmail.com>
2023-03-02 23:01:35 -0400

    Guia 3

---
+++ app/src/main/java/com/example/guias/BackgroundActivity.kt
@@ -0,0 +1,28 @@
+package com.example.guias
+
+import android.graphics.Color
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import androidx.constraintlayout.widget.ConstraintLayout
+
+class BackgroundActivity : AppCompatActivity() {
+    private val btnWhite: Button
+        get() = findViewById(R.id.btn_white)
+    private val btnRed: Button
+        get() = findViewById(R.id.btn_white)
+    private val layoutPrincipal: ConstraintLayout
+        get() = findViewById(R.id.layoutPrincipal)
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_background)
+
+        btnRed.setOnClickListener {
+            layoutPrincipal.setBackgroundColor(Color.RED)
+        }
+
+        btnWhite.setOnClickListener {
+            layoutPrincipal.setBackgroundColor(Color.WHITE)
+        }
+    }
+}
